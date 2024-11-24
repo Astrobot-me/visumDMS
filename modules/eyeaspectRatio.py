0d4087daaa7860c697ce27930f4039897a866a8b
Astrobot <autobot268@gmail.com>
2024-11-25 00:08:21 +0530

    Ear init

---
+++ modules/eyeaspectRatio.py
@@ -0,0 +1,12 @@
+class EyeAspectRatio:
+
+    def __init__(self,facial_landmarks):
+        self.p1_RIGHT = face[33]
+        self.p2_RIGHT = face[133]
+        self.p3_RIGHT = face[144]
+        p4_RIGHT = face[160]
+        p5_RIGHT = face[153]
+        p6_RIGHT = face[158] 
+
+    def getEARs(self,facial_landmarks,image):
+        pass
