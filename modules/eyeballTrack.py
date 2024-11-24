0e0fa19c3a891e247e15d357025d564e483bba84
Astrobot <autobot268@gmail.com>
2024-11-24 22:36:37 +0530

    Eyeball Track init

---
+++ modules/eyeballTrack.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2,time 
+from collections import deque
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+class Eyeball:
+
+    def __init__(self):
+        pass
+
+
