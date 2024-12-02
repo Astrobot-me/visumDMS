052cabfac3b8869161e6ddefed1583592813c322
Astrobot <autobot268@gmail.com>
2024-12-02 23:06:15 +0530

    resources > audio

---
+++ audioAlert.py
@@ -0,0 +1,51 @@
+from pygame import mixer 
+import time
+
+# Initialize the mixer
+mixer.init()
+
+
+SeatbeltWarn = mixer.Sound("./resources/audio/tesla_seatbelt_warning.mp3")
+Seatbelt = mixer.Sound("./resources/audio/tesla_seatbelt.mp3")
+MasterCaution = mixer.Sound("./resources/audio/alert.mp3")
+HazardTone = mixer.Sound("./resources/audio/alert.mp3")
+
+channel1 = mixer.Channel(0)
+channel1.set_volume(1.0)
+
+channel2 = mixer.Channel(1)
+channel2.set_volume(1.0)
+
+channel3 = mixer.Channel(2)
+
+def playAlarm(state: int,seatbelt = "SEATBELT"):
+    if(state == 0):
+        pass
+        # channel1.play(MasterCaution)
+    elif(state == 1):
+        channel1.play(MasterCaution)
+    elif(state == 2):
+        channel3.play(HazardTone)
+
+    if(seatbelt == "SEATBELT"):
+        playSeatbeltWarn(True)
+    else:
+        pass
+
+
+def playSeatbeltWarn(play:True):
+
+    if(play):
+        channel2.play(SeatbeltWarn)
+    else:
+        channel2.play(Seatbelt)
+        
+
+    while play:
+        if not channel2.get_busy():
+            channel2.play()
+
+        time.sleep(0.1)
+
+
+    
--- traio.py
+++ traio.py
@@ -26,3 +26,75 @@ if(size>1):
 
 
 print(len({}.keys()))        
+
+from pygame import mixer 
+
+# Starting the mixer 
+mixer.init() 
+
+# Loading the song 
+mixer.music.load("./resources/audio/alert.mp3") 
+
+# Setting the volume 
+mixer.music.set_volume(1) 
+
+# Start playing the song 
+mixer.music.play() 
+
+# Infinite loop for user interaction
+while True: 
+    print("Press 'p' to pause, 'r' to replay from the start") 
+    print("Press 'e' to exit the program") 
+    query = input(" ").strip().lower()  # Convert input to lowercase and remove extra spaces
+    
+    if query == 'p': 
+        # Pausing the music 
+        mixer.music.pause()	      
+    elif query == 'r': 
+        # Rewind and replay the music 
+        mixer.music.rewind() 
+        mixer.music.play()
+    elif query == 'e': 
+        # Stop the mixer and exit
+        mixer.music.stop() 
+        break
+    else:
+        print("Invalid input. Please try again.")
+
+# Load the first audio file
+audio1 = mixer.Sound("./resources/audio/alert.mp3") 
+
+# Load the second audio file
+audio2 = mixer.Sound("./resources/audio/tesla_seatbelt.mp3") 
+
+# Play both audio files on different channels
+channel1 = mixer.Channel(0)  # First audio on channel 0
+channel2 = mixer.Channel(1)  # Second audio on channel 1
+
+channel1.play(audio1)
+channel2.play(audio2)
+
+# Infinite loop for user interaction
+while True: 
+    print("Press 'p' to pause both audios, 'r' to replay both from the start") 
+    print("Press 'e' to exit the program") 
+    query = input(" ").strip().lower()
+    
+    if query == 'p': 
+        # Pausing both audios
+        channel1.pause()
+        channel2.pause()
+    elif query == 'r': 
+        # Rewind and replay both audios
+        channel1.stop()
+        channel2.stop()
+        channel1.play(audio1)
+        channel2.play(audio2)
+    elif query == 'e': 
+        # Stop both audios and exit
+        channel1.stop()
+        channel2.stop()
+        break
+    else:
+        print("Invalid input. Please try again.") 
+
